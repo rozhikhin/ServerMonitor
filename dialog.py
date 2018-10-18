3844f9536f4db7b2ea6034c68282fa1d8a06a6a9
Rozhikhin Alexandr <rozhihin@gmail.com>
2018-10-18 18:51:54 +0300

    check valid enter

--- dialog.py
+++ dialog.py
@@ -2,20 +2,14 @@ from tkinter import *
 from tkinter import ttk
 
 class Dialog(Frame, object):
-    def __init__(self, master, server="", state="", edit=False):
+    def __init__(self, master, server="", state="", title="Добавить запись"):
         Frame.__init__(self, master)
         self.master = master
-        # self.edit = edit
         self.toplevel_dialog = Toplevel()
         self.toplevel_dialog.minsize(300, 100)
         self.toplevel_dialog.protocol("WM_DELETE_WINDOW", self.close_toplevel)
         self.toplevel_dialog.transient(master)
-
-        if edit:
-            self.toplevel_dialog.title("Редактировать запись")
-        else:
-            self.toplevel_dialog.title("Добавить запись")
-
+        self.toplevel_dialog.title(title)
         self.toplevel_dialog.grab_set()
         self.toplevel_dialog.focus_set()
 
@@ -23,7 +17,8 @@ class Dialog(Frame, object):
         container_server.pack(fill='x', side=TOP, pady=10, padx=(10, 10))
         self.toplevel_dialog_label = ttk.Label(container_server, text='Сервер')
         self.toplevel_dialog_label.pack(side=LEFT)
-        self.toplevel_entry_server = Entry(container_server, width=30)
+        # Проверка - если виджет получил фокус, то вызвать функцию - set_default_style, которая вернет виджету фон по-умолчанию
+        self.toplevel_entry_server = Entry(container_server, width=33,  validate='focus', validatecommand=self.set_default_style)
         self.toplevel_entry_server.insert(0, server)
         self.toplevel_entry_server.pack(side=RIGHT)
 
@@ -31,9 +26,9 @@ class Dialog(Frame, object):
         container_state.pack(fill='x', side=TOP, pady=10, padx=(10, 10))
         self.toplevel_dialog_label = ttk.Label(container_state, text='Состояние')
         self.toplevel_dialog_label.pack(side=LEFT)
-        self.toplevel_entry_state = Entry(container_state, width=30)
-        self.toplevel_entry_state.insert(0,state)
-        self.toplevel_entry_state.pack(side=RIGHT)
+        self.toplevel_combobox_state = ttk.Combobox(container_state, width=30, values=("OK", "ERROR"), state='readonly')
+        self.toplevel_combobox_state.current(0)
+        self.toplevel_combobox_state.pack(side=RIGHT)
 
         container_buttons = ttk.Frame(self.toplevel_dialog)
         container_buttons.pack(fill='x', side=TOP, pady=10, padx=(10, 10))
@@ -43,11 +38,26 @@ class Dialog(Frame, object):
         self.toplevel_dialog_no_button.pack(side=LEFT, fill='x', expand=True)
 
     def get_text(self):
-        server_name = self.toplevel_entry_server.get()
-        state = self.toplevel_entry_state.get()
+
+        # Получаем имя сервера и приводим его к верхнему регистру
+        server_name = self.toplevel_entry_server.get().upper().strip()
+        # Если поле с именем сервера пустое, то кстановить фон поля ввода красного цвета и остановить обработку
+        if not server_name:
+            self.toplevel_entry_server.config({"background": "Red"})
+            return
+        state = self.toplevel_combobox_state.get()
+        # Вызываем функцию главного модуля и сохраняем данные
         self.master.save_server({"server_name": server_name, "state": state})
         self.close_toplevel()
 
+    # Вернуть виджету фон по умолчанию (если он получил фокус)
+    def set_default_style(self):
+        # self.toplevel_entry_server.config({"background": "White"})
+        self.toplevel_entry_server.configure(bg="White")
+        # Метод проверки должен вернуть True, чтобы разрешить изменение (т.е. проверка будет выпоняться каждый раз при наступлении события,
+        # False, чтобы отклонить его ( т.е. проверка не будет выполняться)
+        # или None, чтобы отключить себя (проверка будет выпонена 1 раз и потом будет оключена).
+        return True
 
     def close_toplevel(self):
         self.toplevel_dialog.destroy()
--- monitor.py
+++ monitor.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from dialog import Dialog
 from tkinter import messagebox
 from sqlapi import DB
+import re
 
 class Table(Frame, object):
     def __init__(self, master):
@@ -11,7 +12,6 @@ class Table(Frame, object):
         # Таблица со списком серверов
         self.tree = None
         # Дочернее окно для редактирования и добавления записей
-        # self.dialog = None
         # Данные, приходящие из дочернего окна
         self.toplevel_data = None
         # Редактируется ли запись (self.edit = True) или создается новая (self.edit = False)
@@ -19,13 +19,9 @@ class Table(Frame, object):
         self.servers = list()
         self.sqlapi = DB()
         self.sqlapi.init_db()
-
-        self.servers.sort()
-
         self.setup_ui()
         self.add_all_servers()
         self.get_settings()
-
 
     def setup_ui(self):
 
@@ -45,19 +41,26 @@ class Table(Frame, object):
         padx_button = 25
         container2 = ttk.Frame()
         container2.pack(fill='x', side=TOP, pady=10, padx=10)
-        button_exit = Button(container2, text="Изменить", width=15, command=self.edit_server)
+        button_exit = Button(container2, text="Изменить", width=15, command=self.create_top_for_edit_server)
         button_exit.pack(side=LEFT, padx=(0,padx_button))
-        button_add = Button(container2, text="Добавить", width=15, command=self.add_server)
+        button_add = Button(container2, text="Добавить", width=15, command=self.create_top_for_add_server)
         button_add.pack(side=LEFT, padx=(0,padx_button))
         button_del = Button(container2, text="Удалить", width=15, command=self.remove_server)
         button_del.pack(side=LEFT, padx=(0,padx_button))
+
+        container_info = ttk.Frame()
+        container_info.pack(fill='x', side=TOP, pady=5, padx=(10, 10))
+        self.label_message = Label(container_info, text="", fg='Red')
+        self.label_message.pack_forget()
+        # self.label_message_interval.pack(side=LEFT, padx=(100, 0))
 
         container_interval = ttk.Frame()
         container_interval.pack(fill='x', side=TOP, pady=5, padx=(10, 10))
         label_interval = Label(container_interval, text="Интервал")
         label_interval.pack( side=LEFT, padx=5)
-        self.entry_interval = Entry(container_interval, width=10)
+        self.entry_interval = Entry(container_interval, width=10, validate='focusin', validatecommand=self.set_default_style)
         self.entry_interval.pack( side=RIGHT, padx=(0, 200))
+
 
         container_numcheck = ttk.Frame()
         container_numcheck.pack(fill='x', side=TOP, pady=5, padx=(10, 10))
@@ -79,8 +82,8 @@ class Table(Frame, object):
         container3.pack(fill='x', side=TOP, pady=10, padx=10)
         button_exit = Button(container3, text="Выйти", width=15, command=root.quit)
         button_exit.pack(side=RIGHT, padx=(0, padx_button))
-        button_save = Button(container3, text="Сохранить", width=15, command=self.save_settings)
-        button_save.pack(side=RIGHT, padx=(0, padx_button))
+        self.button_save = Button(container3, text="Сохранить", width=15, command=self.save_settings)
+        self.button_save.pack(side=RIGHT, padx=(0, padx_button))
         button_del = Button(container3, text="Отмена", width=15)
         button_del.pack(side=RIGHT, padx=(0, padx_button))
 
@@ -90,51 +93,107 @@ class Table(Frame, object):
         return {'server_name': server_name, 'state': state}
 
     def add_all_servers(self):
-        self.servers = self.sqlapi.get_servers()
-        if self.servers is not None:
-            for values in self.servers:
+        servers = self.sqlapi.get_servers()
+        servers.sort()
+        if servers is not None:
+            for values in servers:
                 self.tree.insert("", END, values=values)
 
-    def add_server(self):
+    def create_top_for_add_server(self):
         self.edit = False
-        self.create_toplevel()
+        Dialog(self)
 
-    def save_server(self, data):
-        if self.edit:
-            self.tree.delete(self.tree.selection())
-        self.tree.insert("", END, values=(data["server_name"], data["state"]))
-        self.sqlapi.save_server(data)
-
-        servers = []
-        for item in self.tree.get_children():
-            server, state = self.tree.item(item, 'values')
-            servers.append((server, state))
-            servers.sort()
-        self.tree.delete(*self.tree.get_children())
-        for values in servers:
-            self.tree.insert("", END, values=values)
-
-    def edit_server(self):
+    def create_top_for_edit_server(self):
         self.edit = True
         server, state = ('', '')
         for nm in self.tree.selection():
             server, state = self.tree.item(nm, 'values')
         if server:
-            self.create_toplevel(server, state)
+            dialog = Dialog(self, server, state, title="Редактировать запись")
         else:
             messagebox.showinfo("Выберите сервер", "Необходимо выбрать сервер для редактирования")
 
+    def add_server(self, server_data):
+        """
+        Функция add_server принимает словарь из имени сервера и состояния.
+        Сохраняет новую запись в базе и добавляет ее в список серверов в приложении и сортирует его
+        :param server_data: dict
+        :return: None
+        """
+        # Проверяем, что сохранение в базу данные прошло успешно
+        if self.sqlapi.save_server(server_data):
+            self.sort_serevrs_list(server_data)
+
+    def edit_server(self, server_data):
+        """
+        Функция edit_server принимает словарь из имени сервера и состояния.
+        Сохраняет измененную запись в базе и добавляет ее в список серверов в приложении и сортирует его
+        :param server_data: dict
+        :return: None
+        """
+        # Добавим к словарю старое имя сервера, которое нужно заменить
+        server_data['old_name'] = self.select()['server_name']
+        # Удаляем из списка старое имя сервера
+        self.tree.delete(self.tree.selection())
+        if self.sqlapi.update_server(server_data):
+            self.sort_serevrs_list(server_data)
+
+    def sort_serevrs_list(self, server_data):
+        # Создаем пустой список
+        servers = list()
+        # Добавляем в список новый сервер
+        servers.append((server_data["server_name"], server_data["state"]))
+        # Получаем и перебираем все элементы в списке серверов.
+        for item in self.tree.get_children():
+            server, state = self.tree.item(item, 'values')
+            # Проверяем, что новое имя сервера уникально - для новой записи
+            if (not self.edit) and (server_data["server_name"] == server):
+                return
+            # Добавляем каждую запись в список
+            servers.append((server, state))
+        # Сортируем список
+        servers.sort()
+        # Удаляем все записи из списка серверов
+        self.tree.delete(*self.tree.get_children())
+        # И снова добавляем их в отсортированном виде
+        for values in servers:
+            self.tree.insert("", END, values=values)
+
+
+    def save_server(self, server_data):
+        if self.edit:
+            self.edit_server(server_data)
+        else:
+            self.add_server(server_data)
+
     def remove_server(self):
-        # self.tree.delete(*self.tree.get_children())
         select_server = self.select()
         self.tree.delete(self.tree.selection())
         self.sqlapi.delete_server(select_server)
 
     def save_settings(self):
-        self.sqlapi.update_settings({"interval": self.entry_interval.get(),
-                                  "count_of_check": self.entry_numcheck.get(),
-                                   "email": self.entry_email.get()
-                                     })
+        msg = 'Неверно заполнено поле '
+        error = False
+        entry_text_dict = {
+                        "interval": self.entry_interval.get(),
+                        "count_of_check": self.entry_numcheck.get(),
+                        "email": self.entry_email.get()
+        }
+        if not entry_text_dict["interval"].isnumeric():
+            self.entry_interval.config({"background": "Red"})
+            error = True
+        if not entry_text_dict["count_of_check"].isnumeric():
+            self.entry_numcheck.config({"background": "Red"})
+            error = True
+        if not self.check_email(entry_text_dict["email"]):
+            self.entry_email.config({"background": "Red"})
+            error = True
+        if error:
+            self.button_save.focus()
+            self.label_message.pack(side=LEFT, padx=(100, 0))
+            self.label_message.config(text=msg)
+            return
+        self.sqlapi.update_settings(entry_text_dict)
         self.quit()
 
     def get_settings(self):
@@ -143,8 +202,24 @@ class Table(Frame, object):
         self.entry_numcheck.insert(0, str(count_of_check))
         self.entry_email.insert(0, email)
 
-    def create_toplevel(self, server='', state=''):
-        Dialog(self, server, state, self.edit)
+    # Вернуть виджету фон по умолчанию (если он получил фокус)
+    def set_default_style(self):
+        # self.toplevel_entry_server.config({"background": "White"})
+        self.entry_interval.configure(bg="White")
+        self.entry_numcheck.configure(bg="White")
+        self.entry_email.configure(bg="White")
+        # Метод проверки должен вернуть True, чтобы разрешить изменение (т.е. проверка будет выпоняться каждый раз при наступлении события,
+        # False, чтобы отклонить его ( т.е. проверка не будет выполняться)
+        # или None, чтобы отключить себя (проверка будет выпонена 1 раз и потом будет оключена).
+        return True
+
+    def check_email(self, email):
+        exp = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        res = re.search(exp, email)
+        if res:
+            return True
+        return False
+
 
 if __name__ == '__main__':
     root = Tk()
