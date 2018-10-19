5dbd6f4014f8bda9f45f1d00385c2ec5fcaff4d8
Rozhikhin Alexandr <rozhihin@gmail.com>
2018-10-19 18:29:50 +0300

    add docstrings

--- dialog.py
+++ dialog.py
@@ -3,16 +3,27 @@ from tkinter import ttk
 
 class Dialog(Frame, object):
     def __init__(self, master, server="", state="", title="Добавить запись"):
+        """
+        Функция __init__ инициализирует создание экземпляра класса Dialog и создает пользовательский интерфейс
+        :param master: Table
+        :param server: str
+        :param state: str
+        :param title: str
+        """
         Frame.__init__(self, master)
+        # Создание экземпляра класса Toplevel
         self.master = master
         self.toplevel_dialog = Toplevel()
         self.toplevel_dialog.minsize(300, 100)
         self.toplevel_dialog.protocol("WM_DELETE_WINDOW", self.close_toplevel)
         self.toplevel_dialog.transient(master)
+        # Установка заголовка окна (зависит от того, редактируется запись или создается новая
         self.toplevel_dialog.title(title)
+        # Сделать окно модальным
         self.toplevel_dialog.grab_set()
         self.toplevel_dialog.focus_set()
 
+        # Контейнер с надписью и полем для имени сервера
         container_server = ttk.Frame(self.toplevel_dialog)
         container_server.pack(fill='x', side=TOP, pady=10, padx=(10, 10))
         self.toplevel_dialog_label = ttk.Label(container_server, text='Сервер')
@@ -22,6 +33,7 @@ class Dialog(Frame, object):
         self.toplevel_entry_server.insert(0, server)
         self.toplevel_entry_server.pack(side=RIGHT)
 
+        # Контейнер с надписью и выпадающим спискок для указания состояния сервера
         container_state = ttk.Frame(self.toplevel_dialog)
         container_state.pack(fill='x', side=TOP, pady=10, padx=(10, 10))
         self.toplevel_dialog_label = ttk.Label(container_state, text='Состояние')
@@ -30,6 +42,7 @@ class Dialog(Frame, object):
         self.toplevel_combobox_state.current(0)
         self.toplevel_combobox_state.pack(side=RIGHT)
 
+        # Контейнер для кнопок
         container_buttons = ttk.Frame(self.toplevel_dialog)
         container_buttons.pack(fill='x', side=TOP, pady=10, padx=(10, 10))
         self.toplevel_dialog_yes_button = ttk.Button(container_buttons, text='Сохранить', command=self.get_text)
@@ -38,7 +51,10 @@ class Dialog(Frame, object):
         self.toplevel_dialog_no_button.pack(side=LEFT, fill='x', expand=True)
 
     def get_text(self):
-
+        """
+        Функция get_text получает данные из полей формы и сохраняет данные
+        :return: None
+        """
         # Получаем имя сервера и приводим его к верхнему регистру
         server_name = self.toplevel_entry_server.get().upper().strip()
         # Если поле с именем сервера пустое, то кстановить фон поля ввода красного цвета и остановить обработку
@@ -50,9 +66,11 @@ class Dialog(Frame, object):
         self.master.save_server({"server_name": server_name, "state": state})
         self.close_toplevel()
 
-    # Вернуть виджету фон по умолчанию (если он получил фокус)
     def set_default_style(self):
-        # self.toplevel_entry_server.config({"background": "White"})
+        """
+        Функция set_default_style возвращает виджету фон по умолчанию (если он получил фокус)
+        :return: bool
+        """
         self.toplevel_entry_server.configure(bg="White")
         # Метод проверки должен вернуть True, чтобы разрешить изменение (т.е. проверка будет выпоняться каждый раз при наступлении события,
         # False, чтобы отклонить его ( т.е. проверка не будет выполняться)
@@ -60,6 +78,10 @@ class Dialog(Frame, object):
         return True
 
     def close_toplevel(self):
+        """
+        Функция close_toplevel закрывает окно
+        :return: None
+        """
         self.toplevel_dialog.destroy()
 
 
